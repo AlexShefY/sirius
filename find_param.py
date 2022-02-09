097c252c9d92966146361f3d28606396ce212d7c
AlexShefY <alex_shef_31@mail.ru>
2022-02-09 22:11:09 +0300

    trying to find params

---
+++ find_param.py
@@ -0,0 +1,31 @@
+import torch
+import pickle
+from models import M5, resNet, denseNet, CnnFnnModel, ModifiedResNet
+from train_one_model import def_train_one_model
+from different_param import def_different_param
+
+import optuna
+from data import project, run, config, device
+
+from data import build_dataloader
+from data import project, run, config, device, lr, nus_first, nus_second, betas_first, betas_second, gamma
+
+train_dataloader, val_dataloader, test_dataloader = build_dataloader()
+
+def get_accuracy(trial):
+  model = CnnFnnModel()
+  model = model.to(device)
+  lr = trial.suggest_float('lr', 1e-4, 1e-1)
+  nus_first = trial.suggest_float('nus_first', 0.1, 1.0)
+  nus_second = trial.suggest_float('nus_second', 0.1, 1.0)
+  betas_first = trial.suggest_float('betas_first', 0.9, 1.0)
+  betas_second = trial.suggest_float('betas_second', 0.9, 1.0)
+  gamma = trial.suggest_float('gamma', 0.8, 1.0)
+  return def_train_one_model(model, train_dataloader, val_dataloader, test_dataloader, epochs=5, flag=False)
+
+import optuna
+
+study = optuna.create_study()
+study.optimize(get_accuracy, n_trials=100)
+
+print(study.best_params)
--- train_one_model.py
+++ train_one_model.py
@@ -4,10 +4,11 @@ from torch.optim.lr_scheduler import ExponentialLR
 from torch import nn
 import torch
 from tqdm import tqdm
+from math import log
 
 from data import project, run, config, device, lr, nus_first, nus_second, betas_first, betas_second, gamma
 
-def def_train_one_model(model, train_dataloader, val_dataloader, test_dataloader):
+def def_train_one_model(model, train_dataloader, val_dataloader, test_dataloader, epochs=50, flag=True):
 	run['lr'] = lr
 	run['nus_first'] = nus_first
 	run['nus_second'] = nus_second
@@ -18,8 +19,6 @@ def def_train_one_model(model, train_dataloader, val_dataloader, test_dataloader
 
 	scheduler = ExponentialLR(optimizer = optim, gamma = gamma)
 
-	epochs = 50
-
 	loss = nn.CrossEntropyLoss()
 	import time
 	import random 
@@ -27,8 +26,9 @@ def def_train_one_model(model, train_dataloader, val_dataloader, test_dataloader
 	random.seed(time.time())
 	t = random.randint(1, 1000000)
 	for epoch in tqdm(range(epochs)):
-	    train(train_dataloader, epoch, model, optim, loss)
-	    test(val_dataloader,epoch + 1, model, loss)
+	    train(train_dataloader, epoch, model, optim, loss, flag)
+	    test(val_dataloader,epoch + 1, model, loss, flag)
 	    scheduler.step()
 	    torch.save(model, f'models_rubbish/model{t}_{epoch + 1}.pt')
 	    run[f'models_rubbish/model{t}_{epoch + 1}.pt'].upload(f'models_rubbish/model{t}_{epoch + 1}.pt')
+	return -log(test(val_dataloader, epochs + 1, model, loss, flag)[0])
