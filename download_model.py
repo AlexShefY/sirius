abcd909b3ff18150f7b9f5966af1c92b0b1bf1a7
AlexShefY <alex_shef_31@mail.ru>
2022-02-13 13:57:32 +0300

    change downloading models

--- download_model.py
+++ download_model.py
@@ -1,12 +1,22 @@
 import neptune.new as neptune
 
-run_number = 841
-model_name = 'model418348_34'
+def one_model(run_nunmber, model_name):
+    run_ = neptune.init(
+        project='lora0207/sirius',
+        api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZmQyMjc4Ni02NWQwLTRiZTYtYWIyZC0yOGJjOTE2NDNmODEifQ==',
+        run=f'SIR-{run_number}' # for example 'SAN-123'
+        )
 
-run_ = neptune.init(
-    project='lora0207/sirius',
-    api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZmQyMjc4Ni02NWQwLTRiZTYtYWIyZC0yOGJjOTE2NDNmODEifQ==',
-    run=f'SIR-{run_number}' # for example 'SAN-123'
-    )
+    run_[f'models_rubbish/{model_name}.pt'].download('models')
 
-run_[f'models_rubbish/{model_name}.pt'].download('models')
+def all_models(run_number):
+    run_ = neptune.init(
+        project='lora0207/sirius',
+        api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkZmQyMjc4Ni02NWQwLTRiZTYtYWIyZC0yOGJjOTE2NDNmODEifQ==',
+        run=f'SIR-{run_number}' # for example 'SAN-123'
+        )
+
+    run_[f'models'].download()
+
+if __name__ == "__main__":
+    all_models()
